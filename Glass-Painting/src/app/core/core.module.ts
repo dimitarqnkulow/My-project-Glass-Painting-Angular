de525045815feb4859dae63a3c5c35c0da6bb5ef
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2023-07-21 14:16:36 +0300

    Add core module with footer and header components

--- Glass-Painting/src/app/core/core.module.ts
+++ Glass-Painting/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AppRoutingModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
